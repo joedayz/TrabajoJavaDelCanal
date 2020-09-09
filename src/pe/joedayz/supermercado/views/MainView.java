1c60750427ec1e336249899e896f5eb2482c21d3
Jose Diaz <jadiaz@farmaciasperuanas.pe>
2020-09-09 15:43:18 -0500

    Mejorando el MainView

--- src/pe/joedayz/supermercado/views/MainView.java
+++ src/pe/joedayz/supermercado/views/MainView.java
@@ -10,12 +10,37 @@ public class MainView extends JFrame {
 	private JPanel panelPrincipal;
 
 	public MainView(){
-		this.setContentPane(this.panelPrincipal);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.pack();
+		initComponents();
 
 		createUIComponents();
 
+	}
+
+	private void initComponents() {
+		setContentPane(this.panelPrincipal);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Supermercado");
+		setMaximumSize(new java.awt.Dimension(800, 600));
+		setMinimumSize(new java.awt.Dimension(800, 600));
+
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(
+				layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(layout.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(tabMain, javax.swing.GroupLayout.DEFAULT_SIZE, 776, Short.MAX_VALUE)
+								.addContainerGap())
+		);
+		layout.setVerticalGroup(
+				layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(layout.createSequentialGroup()
+								.addContainerGap()
+								.addComponent(tabMain, javax.swing.GroupLayout.DEFAULT_SIZE, 576, Short.MAX_VALUE)
+								.addContainerGap())
+		);
+
+		this.pack();
 	}
 
 	private void createUIComponents() {
