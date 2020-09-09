c6f9b3974fd7793bccb22e13411e9c4fafec4144
Jose Diaz <jadiaz@farmaciasperuanas.pe>
2020-09-09 15:35:04 -0500

    Nimbus look and feel

--- src/pe/joedayz/supermercado/Main.java
+++ src/pe/joedayz/supermercado/Main.java
@@ -5,6 +5,17 @@ import pe.joedayz.supermercado.views.MainView;
 public class Main {
 
     public static void main(String[] args) {
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info:javax.swing.UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException | InstantiationException |
+				IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+			ex.printStackTrace();
+		}
 		java.awt.EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				new MainView().setVisible(true);
